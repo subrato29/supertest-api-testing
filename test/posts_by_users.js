172c343638b234314fa319bbbc188b5d1f76a325
Subrato Sarkar <subratosarkar@Subratos-MacBook-Pro.local>
2021-09-21 11:14:57 -0400

    adding tests

---
+++ test/posts_by_users.js
@@ -0,0 +1,23 @@
+'use strict';
+
+import { expect } from 'chai';
+import request from '../config/common';
+
+const urls = require('../config/urls.js');
+const apiUtils = require('../lib/apiUtils.js');
+
+describe('Posts by users', () => {
+    it('GET /retrieve all posts: ', () => {
+        const endpoint = urls.endpoint.posts.list_of_all_post;
+        return apiUtils.get(request, endpoint).then((response) => {
+            const statusCode = response.status;
+            if(statusCode === 200) {
+                return expect(response.body.meta.pagination.total).to.be.greaterThan(1000); 
+            } else {
+                throw new Error('Unexpected status code: ' + statusCode);
+            }
+       }).catch((err) => {
+           return Promise.reject(err);
+       });
+    });
+})
