bfc42caa3206fa6fe354afd40c20da44e380b9d0
Subrato Sarkar <subratosarkar@Subratos-MacBook-Pro.local>
2021-09-21 01:07:16 -0400

    added tests on GET

--- config/urls.js
+++ config/urls.js
@@ -4,7 +4,10 @@ module.exports = {
     baseUrl : 'https://gorest.co.in',
     endpoint: {
         users: {
-            list_of_all_users: '/public/v1/users',
+            list_of_all_users: '/public/v1/users'
+        },
+        posts: {
+            list_of_all_post: '/public/v1/posts'
         }
     }
 };
--- test/GET.js
+++ test/GET.js
@@ -7,12 +7,26 @@ const urls = require('../config/urls.js');
 const apiUtils = require('../lib/apiUtils.js');
 const data = require('../data/data.js');
 
-describe('Users', () => {
+describe('GET', () => {
     it('GET /retrieve all users: ', () => {
         const endpoint = urls.endpoint.users.list_of_all_users;
         return apiUtils.get(request, endpoint).then((response) => {
             const statusCode = response.status;
             if(statusCode === 200) { 
+                return expect(response.body.meta.pagination.total).to.be.greaterThan(1000); 
+            } else {
+                throw new Error('Unexpected status code: ' + statusCode);
+            }
+       }).catch((err) => {
+           return Promise.reject(err);
+       });
+    });
+
+    it('GET /retrieve all posts: ', () => {
+        const endpoint = urls.endpoint.posts.list_of_all_post;
+        return apiUtils.get(request, endpoint).then((response) => {
+            const statusCode = response.status;
+            if(statusCode === 200) {
                 return expect(response.body.meta.pagination.total).to.be.greaterThan(1000); 
             } else {
                 throw new Error('Unexpected status code: ' + statusCode);
